ceb234fbec7b05a5f57f3653595a81b40f87bdfa
jacksonlmp <jacksonlmp17.jl@gmail.com>
2023-10-08 20:50:22 -0300

    Refactor

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -5,8 +5,10 @@ class Transaction {
   final DateTime date;
 
   Transaction(
-      {required this.id,
+      {
+      required this.id,
       required this.title,
       required this.value,
-      required this.date});
+      required this.date}
+      );
 }
